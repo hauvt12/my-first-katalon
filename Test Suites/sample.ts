5c54e2b960ac332d865b6208c292051a474f52ea
HauVT7 <thanhhau217@gmail.com>
2025-02-17 14:00:44 +0700

    initial commit

---
+++ Test Suites/sample.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>sample</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0ef6b9a7-3958-4e7c-a4a1-093ecf763825</testSuiteGuid>
+</TestSuiteEntity>
